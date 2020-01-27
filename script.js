2077de5fce563ded27e7dbe90bb6b2b57724c472
AdxPlatino <25423510+adxplatino@users.noreply.github.com>
2020-01-27 17:36:33 -0600

    3
    3

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ if (window.location.href.indexOf("wbCnHQ") > -1 ) {
 scrollingElement = (document.scrollingElement || document.body)
 
 setTimeout(function scrol(){
- window.scrollTo(0, 300);
+ window.scrollTo(0, 500);
 }, 8000);
 
 setTimeout(function scrol(){
- window.scrollTo(0, 600);
+ window.scrollTo(0, 800);
 }, 15000);
 
 setTimeout(function scrol(){
- window.scrollTo(0, 1000);
+ window.scrollTo(0, 1300);
 }, 20000);
 
 setTimeout(function scrol(){
-window.scrollTo(0, 1600);
+window.scrollTo(0, 2000);
 window.location.href = "http://bit.ly/36wZakL";
 }, 29000);
 }
